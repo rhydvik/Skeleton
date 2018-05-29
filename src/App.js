85a0bc374c8ea07df6e6b6e0f679cc12e9d57878
vipul <vipul21sept@gmail.com>
2018-05-29 10:13:13 +0530

    Working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 
-import { Skeleton } from '../node_modules/react-skeleton-loading/src';
-
-const MyLoader = () => <Skeleton />;
-
+import { Skeleton } from 'react-skeleton-loading';
 import './App.css';
+
+const MyLoader = () => <Skeleton height={200} width={200}/>;
+
 
 class App extends Component {
   render() {
